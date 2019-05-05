bd00536f4ea1aa2deccba7edf7f7ba175fbf43c8
Damian Baar <damian.baar@gmail.com>
2019-05-05 18:16:43 +0200

    hydra-setup

---
+++ simple_hydra_vbox.nix
@@ -0,0 +1,25 @@
+{
+  my-hydra = 
+    { config, pkgs, ... }: {
+      # nixpkgs.localSystem.system = "x86_64-linux";
+      # pkgs.localSystem.system = "x86_64-linux";
+
+      deployment.targetEnv                    = "virtualbox";
+
+        virtualisation.virtualbox.guest.enable = true;
+      deployment.virtualbox.memorySize        = 4096;
+      deployment.virtualbox.vcpu              = 2;
+      deployment.virtualbox.headless          = true;
+      services.nixosManual.showManual         = false;
+      services.ntp.enable                     = false;
+      # services.openssh.allowSFTP              = false;
+      # services.openssh.enable = true;
+      # services.openssh.passwordAuthentication = false;
+      # services.openssh.permitRootLogin="yes";
+      users = {
+        mutableUsers = false;
+        users.root.openssh.authorizedKeys.keyFiles = [ ~/.ssh/id_rsa.pub ];
+        # users.damianbaar.openssh.authorizedKeys.keyFiles = [ ~/.ssh/id_rsa.pub ];
+      };
+    };
+}
